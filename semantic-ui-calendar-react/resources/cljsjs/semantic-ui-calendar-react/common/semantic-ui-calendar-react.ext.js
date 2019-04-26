1dee4d1d86ff1d07e2697668d27492fba32edb2f
paul.rutledge <paul.rutledge@vodori.com>
2019-04-26 09:20:18 -0500

    semantic ui calendar react

---
+++ semantic-ui-calendar-react/resources/cljsjs/semantic-ui-calendar-react/common/semantic-ui-calendar-react.ext.js
@@ -0,0 +1,16 @@
+this["semantic-ui-calendar-react"] = {
+    "DateInput": function () {
+    },
+    "DateTimeInput": function () {
+    },
+    "DatesRangeInput": function () {
+    },
+    "TimeInput": function () {
+    },
+    "YearInput": function () {
+    },
+    "MonthInput": function () {
+    },
+    "MonthRangeInput": function () {
+    }
+};
