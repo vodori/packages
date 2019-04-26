632af43a01cd75bc19abaf4ee3fe78c75fe7a461
paul.rutledge <paul.rutledge@vodori.com>
2019-04-26 14:58:59 -0500

    [semantic-ui-calendar-react] Create package
    Creating a cljsjs package for use of semantic-ui-calendar-react component

--- semantic-ui-calendar-react/resources/cljsjs/semantic-ui-calendar-react/common/semantic-ui-calendar-react.ext.js
+++ semantic-ui-calendar-react/resources/cljsjs/semantic-ui-calendar-react/common/semantic-ui-calendar-react.ext.js
@@ -1,16 +1,481 @@
 this["semantic-ui-calendar-react"] = {
-    "DateInput": function () {
+    "DateInput": {
+        "defaultProps": {
+            "dateFormat": {},
+            "icon": {},
+            "inline": {},
+            "localization": {},
+            "preserveViewMode": {},
+            "startMode": {}
+        },
+        "propTypes": {
+            "animation": {
+                "isRequired": function () {}
+            },
+            "clearIcon": {
+                "isRequired": function () {}
+            },
+            "clearable": {
+                "isRequired": function () {}
+            },
+            "closable": {
+                "isRequired": function () {}
+            },
+            "dateFormat": {
+                "isRequired": function () {}
+            },
+            "disable": {
+                "isRequired": function () {}
+            },
+            "duration": {
+                "isRequired": function () {}
+            },
+            "enable": {
+                "isRequired": function () {}
+            },
+            "icon": {
+                "isRequired": function () {}
+            },
+            "iconPosition": {
+                "isRequired": function () {}
+            },
+            "initialDate": {
+                "isRequired": function () {}
+            },
+            "localization": {
+                "isRequired": function () {}
+            },
+            "markColor": {
+                "isRequired": function () {}
+            },
+            "marked": {
+                "isRequired": function () {}
+            },
+            "maxDate": {
+                "isRequired": function () {}
+            },
+            "minDate": {
+                "isRequired": function () {}
+            },
+            "onClear": {
+                "isRequired": function () {}
+            },
+            "preserveViewMode": {
+                "isRequired": function () {}
+            },
+            "startMode": {
+                "isRequired": function () {}
+            },
+            "value": {
+                "isRequired": function () {}
+            }
+        }
     },
-    "DateTimeInput": function () {
+    "DateTimeInput": {
+        "defaultProps": {
+            "dateFormat": {},
+            "divider": {},
+            "icon": {},
+            "inline": {},
+            "localization": {},
+            "preserveViewMode": {},
+            "startMode": {},
+            "timeFormat": {}
+        },
+        "propTypes": {
+            "animation": {
+                "isRequired": function () {}
+            },
+            "clearIcon": {
+                "isRequired": function () {}
+            },
+            "clearable": {
+                "isRequired": function () {}
+            },
+            "closable": {
+                "isRequired": function () {}
+            },
+            "dateFormat": {
+                "isRequired": function () {}
+            },
+            "dateTimeFormat": {
+                "isRequired": function () {}
+            },
+            "disable": {
+                "isRequired": function () {}
+            },
+            "divider": {
+                "isRequired": function () {}
+            },
+            "duration": {
+                "isRequired": function () {}
+            },
+            "icon": {
+                "isRequired": function () {}
+            },
+            "iconPosition": {
+                "isRequired": function () {}
+            },
+            "initialDate": {
+                "isRequired": function () {}
+            },
+            "localization": {
+                "isRequired": function () {}
+            },
+            "markColor": {
+                "isRequired": function () {}
+            },
+            "marked": {
+                "isRequired": function () {}
+            },
+            "maxDate": {
+                "isRequired": function () {}
+            },
+            "minDate": {
+                "isRequired": function () {}
+            },
+            "onClear": {
+                "isRequired": function () {}
+            },
+            "preserveViewMode": {
+                "isRequired": function () {}
+            },
+            "startMode": {
+                "isRequired": function () {}
+            },
+            "timeFormat": {
+                "isRequired": function () {}
+            },
+            "value": {
+                "isRequired": function () {}
+            }
+        }
     },
-    "DatesRangeInput": function () {
+    "DatesRangeInput": {
+        "defaultProps": {
+            "dateFormat": {},
+            "icon": {},
+            "inline": {},
+            "localization": {}
+        },
+        "propTypes": {
+            "allowSameEndDate": {
+                "isRequired": function () {}
+            },
+            "animation": {
+                "isRequired": function () {}
+            },
+            "clearIcon": {
+                "isRequired": function () {}
+            },
+            "clearable": {
+                "isRequired": function () {}
+            },
+            "closable": {
+                "isRequired": function () {}
+            },
+            "dateFormat": {
+                "isRequired": function () {}
+            },
+            "duration": {
+                "isRequired": function () {}
+            },
+            "icon": {
+                "isRequired": function () {}
+            },
+            "iconPosition": {
+                "isRequired": function () {}
+            },
+            "initialDate": {
+                "isRequired": function () {}
+            },
+            "localization": {
+                "isRequired": function () {}
+            },
+            "markColor": {
+                "isRequired": function () {}
+            },
+            "marked": {
+                "isRequired": function () {}
+            },
+            "maxDate": {
+                "isRequired": function () {}
+            },
+            "minDate": {
+                "isRequired": function () {}
+            },
+            "onClear": {
+                "isRequired": function () {}
+            },
+            "value": {
+                "isRequired": function () {}
+            }
+        }
     },
-    "TimeInput": function () {
+    "MonthInput": {
+        "defaultProps": {
+            "dateFormat": {},
+            "icon": {},
+            "inline": {},
+            "localization": {}
+        },
+        "propTypes": {
+            "animation": {
+                "isRequired": function () {}
+            },
+            "clearIcon": {
+                "isRequired": function () {}
+            },
+            "clearable": {
+                "isRequired": function () {}
+            },
+            "closable": {
+                "isRequired": function () {}
+            },
+            "dateFormat": {
+                "isRequired": function () {}
+            },
+            "disable": {
+                "isRequired": function () {}
+            },
+            "duration": {
+                "isRequired": function () {}
+            },
+            "icon": {
+                "isRequired": function () {}
+            },
+            "iconPosition": {
+                "isRequired": function () {}
+            },
+            "initialDate": {
+                "isRequired": function () {}
+            },
+            "localization": {
+                "isRequired": function () {}
+            },
+            "maxDate": {
+                "isRequired": function () {}
+            },
+            "minDate": {
+                "isRequired": function () {}
+            },
+            "onChange": {
+                "isRequired": function () {}
+            },
+            "onClear": {
+                "isRequired": function () {}
+            },
+            "value": {
+                "isRequired": function () {}
+            }
+        }
     },
-    "YearInput": function () {
+    "MonthRangeInput": {
+        "defaultProps": {
+            "dateFormat": {},
+            "icon": {},
+            "inline": {},
+            "localization": {}
+        },
+        "propTypes": {
+            "animation": {
+                "isRequired": function () {}
+            },
+            "clearIcon": {
+                "isRequired": function () {}
+            },
+            "clearable": {
+                "isRequired": function () {}
+            },
+            "closable": {
+                "isRequired": function () {}
+            },
+            "dateFormat": {
+                "isRequired": function () {}
+            },
+            "duration": {
+                "isRequired": function () {}
+            },
+            "icon": {
+                "isRequired": function () {}
+            },
+            "iconPosition": {
+                "isRequired": function () {}
+            },
+            "initialDate": {
+                "isRequired": function () {}
+            },
+            "localization": {
+                "isRequired": function () {}
+            },
+            "maxDate": {
+                "isRequired": function () {}
+            },
+            "minDate": {
+                "isRequired": function () {}
+            },
+            "onClear": {
+                "isRequired": function () {}
+            },
+            "value": {
+                "isRequired": function () {}
+            }
+        }
     },
-    "MonthInput": function () {
+    "TimeInput": {
+        "defaultProps": {
+            "disableMinute": {},
+            "icon": {},
+            "inline": {},
+            "localization": {},
+            "timeFormat": {}
+        },
+        "propTypes": {
+            "animation": {
+                "isRequired": function () {}
+            },
+            "clearIcon": {
+                "isRequired": function () {}
+            },
+            "clearable": {
+                "isRequired": function () {}
+            },
+            "closable": {
+                "isRequired": function () {}
+            },
+            "disableMinute": {
+                "isRequired": function () {}
+            },
+            "duration": {
+                "isRequired": function () {}
+            },
+            "icon": {
+                "isRequired": function () {}
+            },
+            "iconPosition": {
+                "isRequired": function () {}
+            },
+            "localization": {
+                "isRequired": function () {}
+            },
+            "onClear": {
+                "isRequired": function () {}
+            },
+            "timeFormat": {
+                "isRequired": function () {}
+            },
+            "value": {
+                "isRequired": function () {}
+            }
+        }
     },
-    "MonthRangeInput": function () {
+    "YearInput": {
+        "defaultProps": {
+            "dateFormat": {},
+            "icon": {},
+            "inline": {},
+            "localization": {}
+        },
+        "propTypes": {
+            "animation": {
+                "isRequired": function () {}
+            },
+            "clearIcon": {
+                "isRequired": function () {}
+            },
+            "clearable": {
+                "isRequired": function () {}
+            },
+            "closable": {
+                "isRequired": function () {}
+            },
+            "dateFormat": {
+                "isRequired": function () {}
+            },
+            "disable": {
+                "isRequired": function () {}
+            },
+            "duration": {
+                "isRequired": function () {}
+            },
+            "icon": {
+                "isRequired": function () {}
+            },
+            "iconPosition": {
+                "isRequired": function () {}
+            },
+            "initialDate": {
+                "isRequired": function () {}
+            },
+            "localization": {
+                "isRequired": function () {}
+            },
+            "maxDate": {
+                "isRequired": function () {}
+            },
+            "minDate": {
+                "isRequired": function () {}
+            },
+            "onClear": {
+                "isRequired": function () {}
+            },
+            "value": {
+                "isRequired": function () {}
+            }
+        }
     }
+};
+this["semantic-ui-calendar-react"].DateInput.prototype = {
+    "constructor": function () {},
+    "forceUpdate": function () {},
+    "isReactComponent": function () {},
+    "parseInternalValue": function () {},
+    "render": function () {},
+    "setState": function () {}
+};
+this["semantic-ui-calendar-react"].DateTimeInput.prototype = {
+    "constructor": function () {},
+    "forceUpdate": function () {},
+    "getDateTimeFormat": function () {},
+    "getPicker": function () {},
+    "isReactComponent": function () {},
+    "parseInternalValue": function () {},
+    "render": function () {},
+    "setState": function () {}
+};
+this["semantic-ui-calendar-react"].DatesRangeInput.prototype = {
+    "constructor": function () {},
+    "forceUpdate": function () {},
+    "isReactComponent": function () {},
+    "render": function () {},
+    "setState": function () {}
+};
+this["semantic-ui-calendar-react"].MonthInput.prototype = {
+    "constructor": function () {},
+    "forceUpdate": function () {},
+    "isReactComponent": function () {},
+    "render": function () {},
+    "setState": function () {}
+};
+this["semantic-ui-calendar-react"].MonthRangeInput.prototype = {
+    "constructor": function () {},
+    "forceUpdate": function () {},
+    "isReactComponent": function () {},
+    "render": function () {},
+    "setState": function () {}
+};
+this["semantic-ui-calendar-react"].TimeInput.prototype = {
+    "constructor": function () {},
+    "forceUpdate": function () {},
+    "getPicker": function () {},
+    "isReactComponent": function () {},
+    "render": function () {},
+    "setState": function () {}
+};
+this["semantic-ui-calendar-react"].YearInput.prototype = {
+    "constructor": function () {},
+    "forceUpdate": function () {},
+    "isReactComponent": function () {},
+    "render": function () {},
+    "setState": function () {}
 };
